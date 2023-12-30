2befdf4838494b2ee0c4d3eb89ba2449c608edca
Tyler Polito <things@tpolito.com>
2023-12-30 15:39:47 -0600

    init commit

---
+++ new_tab.js
@@ -0,0 +1,111 @@
+function init() {
+  setDateTime();
+  populateLinkGrid();
+}
+
+function setDateTime() {
+  let el = document.querySelector('.dateTime');
+  const date = new Date();
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  const day = days[date.getDay()];
+  const month = months[date.getMonth()];
+  const dateOfMonth = date.getDate();
+
+  el.textContent = `${day}, ${month} ${dateOfMonth}`;
+}
+
+function populateLinkGrid() {
+  const linkContainer = document.querySelector('.linkContainer');
+  const links = [
+    {
+      name: 'Mail',
+      category: 'mail',
+      url: 'https://mail.proton.me/u/0/inbox',
+      icon: './assets/links/mail.png',
+    },
+    {
+      name: 'Calendar',
+      category: 'mail',
+      url: 'https://calendar.proton.me/u/0/',
+      icon: './assets/links/calendar.png',
+    },
+    {
+      name: 'YouTube',
+      category: 'entertainment',
+      url: 'http://www.youtube.com/feed/subscriptions',
+      icon: './assets/links/youtube.png',
+    },
+    {
+      name: 'Twitch',
+      category: 'entertainment',
+      url: 'https://www.twitch.tv/directory/following',
+      icon: './assets/links/twitch.png',
+    },
+    {
+      name: 'tpolito',
+      category: 'development',
+      url: 'https://github.com/tpolito',
+      icon: './assets/links/github.png',
+    },
+    {
+      name: 'StudioTwey',
+      category: 'development',
+      url: 'https://github.com/StudioTwey',
+      icon: './assets/links/github.png',
+    },
+    {
+      name: 'Weather',
+      category: 'other',
+      url: 'https://merrysky.net/forecast/30.2423000,-97.7672000',
+      icon: './assets/links/weather.png',
+    },
+  ];
+
+  // Chunk the links into categories
+  // const categories = {};
+  // links.forEach((link) => {
+  //   if (!categories[link.category]) {
+  //     categories[link.category] = [];
+  //   }
+  //   categories[link.category].push(link);
+  // });
+
+  links.forEach((link) => {
+    const container = document.createElement('a');
+    container.href = link.url;
+    container.classList.add('link');
+    const linkImg = document.createElement('img');
+    const { hostname } = new URL(link.url);
+    linkImg.src = link.icon;
+    container.appendChild(linkImg);
+    const linkName = document.createElement('span');
+    linkName.textContent = link.name;
+    container.appendChild(linkName);
+    linkContainer.appendChild(container);
+  });
+}
+// brave://favicon/size/128@1x/https://news.ycombinator.com/
+init();
